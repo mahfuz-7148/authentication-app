fb3aac695de76ad1d8eab13f9db44de6daadbf11
shanto <mrahman7148@gmail.com>
2026-02-11 21:01:43 +0600

    fix: toast message

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ''
+    ? 'https://authentication-app-75dg.vercel.app'
     : 'http://localhost:5173',
   credentials: true
 }))
